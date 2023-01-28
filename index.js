030661b022ff9a4e3c46f3cc06282c3a81da6551
AgustinRacca <agustinracca00@gmail.com>
2023-01-28 20:59:34 +0100

    Utilizando JavaScript

---
+++ index.js
@@ -0,0 +1,68 @@
+
+let nombre = prompt ("Ingrese su Nombre")
+console.log(nombre);
+
+alert (`Bienvenido a TodoMuebles ${nombre}`);
+
+const productos = [
+    {nombre: "armario", precio: 20000},
+    {nombre: "mueble sala", precio: 30000},
+    {nombre: "sillon", precio: 10000},
+    {nombre: "mesa y sillas", precio: 50000},
+];
+
+let carrito = []
+
+let seleccion = prompt ("¿Desea comprar algun mueble?. Responda si/no?")
+
+while(seleccion != "si" && seleccion != "no"){
+    alert("Por favor ingresa si o no")
+    seleccion = prompt ("¿Hola desea comprar algun mueble?. Responda si/no")
+}
+
+if (seleccion == "si"){
+    alert("A continuacion nuestra lista de productos.")
+    let todoslosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
+    alert(todoslosProductos.join(" - "))
+}else if (seleccion == "no"){
+    alert(`Gracias por visitarnos ${nombre}, hasta la proxima!.`)
+}
+
+while(seleccion != "no"){
+    let producto = prompt("Agrega un producto a tu carrito")
+    let precio = 0
+
+if(producto == "armario" || producto == "mueble sala" || producto == "sillon" || producto == "mesa y sillas"){
+    switch(producto){
+        case "armario":
+        precio = 20000;
+        break
+        case "mueble sala":
+        precio = 30000;
+        break
+        case "sillon":
+        precio = 10000;
+        break
+        case "mesa y sillas":
+        precio = 50000;
+        break
+    }
+
+    let unidades = parseInt(prompt("¿Cuantas unidades quiere llevar?"))
+    carrito.push({producto, unidades, precio})
+}else{
+    alert("No tenemos ese producto")
+}
+
+seleccion = prompt ("¿Desea seguir comprando. Responda si/no?")
+while(seleccion == "no"){
+    alert("Gracias por su compra. Hasta pronto")
+    carrito.forEach((carritoFinal) => {
+        console.log(`producto: ${carritoFinal.producto}, unidades:${carritoFinal.unidades}, precio final ${carritoFinal.unidades * carritoFinal.precio}`)
+    })
+    break;
+}
+}
+
+const total = carrito.reduce ((acc, el) => acc + el.precio * el.unidades, 0)
+console.log (`El total a pagar es: ${total}`)
